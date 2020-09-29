a5d587837bd4cc6eb488bc1f340f422203a3ef8b
Gokul Krishnan <krishgokul351@gmail.com>
2020-09-29 14:27:26 +0530

    added vue build as static file

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,12 @@ const session = require('cookie-session')
 const {sequelize} = require('./models')
 const config = require('./config')
 const passport = require('passport')
+const serveStatic = require('serve-static')
 
 const app = express()
 
+console.log(__dirname + '/public')
+app.use('/', serveStatic(__dirname + '/public'))
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -18,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 //     origin: 'http://localhost:8080'
 // }))
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", 'http://localhost:8080')
+	res.header("Access-Control-Allow-Origin", 'http://localhost:' + config.port)
 	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
 	res.header("Access-Control-Allow-Credentials", true)
 	res.header("Access-Control-Allow-Headers", "content-type")
@@ -42,12 +45,15 @@ const registerRoutes = require('./routes/register')
 const saveRoutes = require('./routes/save')
 const vaultRoutes = require('./routes/vault')
 
-app.get('/', (req, res) => {
-	// console.log(req.user)
-	console.log('isAuth')
-	console.log(req.isAuthenticated())
-	res.send('Hello...')
-})
+// app.get('/', (req, res) => {
+// 	// // console.log(req.user)
+// 	// console.log('isAuth')
+// 	// console.log(req.isAuthenticated())
+// 	// res.send('Hello...')
+// 	// ../../client/dist
+// 	// res.send(__dirname)
+// 	res.sendFile('public/index.html', {root: __dirname})
+// })
 
 app.post('/isAuthenticated', (req, res) => {
 	console.log('isAuth')
